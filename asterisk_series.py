6b46197e63d699616fa80fcc425c27d1a89363ea
Sampath <sg2277@njit.edu>
2020-04-27 04:58:52 -0400

    To Print * for specified series

---
+++ asterisk_series.py
@@ -0,0 +1,8 @@
+# write a program that prints Pattern like this
+#       input : 7
+# * * * * * * *
+
+num_range = int(input("Enter the range: "))
+for i in range(1, num_range + 1):
+    print("* ", end=" ")
+    
